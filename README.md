f14ecdac5e04cde362d2e2a3592017f8f83705f4
Moein Heshmati <moeinheshmati1383@gmail.com>
2024-07-06 00:39:25 +0330

    Upload Exercises-part-1-18 Project On Github

---
+++ README.md
@@ -0,0 +1,27 @@
+Project Name ===> Exercises Part 1
+
+Question Number ===> 18
+
+Programming Language ===> Python
+
+Program description ===> 18th Exercise
+
+Example ===> Write A Program That Adds All The Values In The Above Dictionary Together
+
+A => number = {'a':15, 'b':25, 'c':35, 'd':45}
+
+Description Of Files:
+
+practice_18_linux.py ===> For Linux 
+
+For Run ===> python3 practice_18_linux.py
+
+practice_18_windows.py ===> For Windows
+
+For Run ===> python practice_18_windows.py
+
+Required packages For Python ===> os / termcolor2 / pyfiglet
+
+termcolor2 ===> To install => pip install termcolor2
+
+pyfiglet ===> To install => pip install pyfiglet
---
+++ practice_18_linux.py
@@ -0,0 +1,23 @@
+# Practice_18
+
+# Library
+from os import system as sys
+from termcolor2 import colored
+from pyfiglet import figlet_format
+
+# Clear Terminal
+sys('clear')
+
+# Intro
+print(colored(figlet_format("Exercises Part 1"), color='cyan'))
+print(colored('=====================================================================', color='red'))
+print(colored('Question Number ===> 18', color='blue'))
+print(colored('// Be Sure To Read The README File To See The Question //', color='blue'))
+print('                                            ')
+
+number = {'a':15, 'b':25, 'c':35, 'd':45} 
+
+print(sum(number.values()))
+
+# Finish
+# Create By Moein Heshmati
